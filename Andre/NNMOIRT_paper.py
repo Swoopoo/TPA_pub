855a51f0fa9d8b7c5f1d240930779bb73ea17cfc
Jonathan Spratte <jspratte@yahoo.de>
2018-10-10 11:11:02 +0200

    matrix product XG calculation

--- Andre/NNMOIRT_paper.py
+++ Andre/NNMOIRT_paper.py
@@ -19,7 +19,7 @@ class InitModel:
     def __init__(self, G, C, S):
         self.t = 0
         self.deltat = 0.01
-        self.tau = 1
+        self.tau = 1 # = R_0 C_0
         self.alpha_0 = 7
         self.beta = 2
         self.xi = 5
@@ -33,18 +33,27 @@ class InitModel:
         self.S = S
         self.z, self.deltaz = self.rel()
         self.X = self.smoothMat()
+        self.XG = self.smoothie(G)
+        self.smoothing_weights = (1, -1/8, -1/8)
         self.w = np.array([1/3, 1/3, 1/3])
 
     # Differential von u nach S. 2205 (31)
+    # Ist self.gam hier wirklich das gleiche wie in den anderen Funktionen? Im
+    # Text steht auf S. 2204 das gamma = gamma / C_0 substituiert wird.
     def derivu(self):
-        u_deriv = -(self.u/self.tau) - (self.w[0] * self.gam[0] * (1 + np.log(self.G)) +
-                                        self.w[1] * self.gam[1] * np.dot(self.S.T, self.z) +
-                                        self.w[2] * self.gam[2] * (np.dot(self.X, self.G) + self.G) +
-                                        np.dot(self.S.T, self.deltaz))
+        u_deriv = -(self.u/self.tau) - (
+                  self.w[0] * self.gam[0] * (1 + np.log(self.G))
+                + self.w[1] * self.gam[1] * np.dot(self.S.T, self.z)
+                + self.w[2] * self.gam[2] * (np.dot(self.X, self.G) + self.G)
+                + np.dot(self.S.T, self.deltaz)
+                )
         return u_deriv
 
     # Alpha nach 2204 (29)
     def alpha(self, t):
+        # Spratte: kleiner Fehler, der komische griechische Buchstabe ist ein
+        # zeta: alpha_0, zeta, eta >= 0
+        #return self.alpha_0 + self.zeta * np.exp(-self.eta * t)
         return self.alpha_0 + self.xi * np.exp(-t)
 
     # G(t+deltat) bzw. u(t+deltat) nach S. 2205 (36) bzw. (33)
@@ -59,18 +68,28 @@ class InitModel:
         self.u, self.G = self.delta()
 
     # z und deltaz nach S. 2204 (28) und (27)
+    # Spratte: Müsste ich mir zwar nochmal ansehen, aber das müsste auch ohne
+    # doppelte for-Schleife implementierbar sein (for-Schleifen sind teuer in
+    # Python, die numpy-Routinen sind wesentlich schneller).
+    # z müsste doch eigentlich
+    #    z = np.dot(self.S, self.G) - self.C
+    # sein. Dementsprechend wäre die Funktion hier dann:
+    #def rel(self):
+        #z = np.dot(self.S, self.G) - self.C
+        #deltaz = self.alpha(self.t) * z
+        #deltaz[np.where(deltaz < 0)] = 0
+    # Ich bin mir nicht sicher, ob die Implementierung oben wirklich optimal
+    # ist oder ob es schneller ginge, nähme man eine for-Schleife für die
+    # Berechnung von deltaz.
     def rel(self): # delta(z) Berechnung
         z = np.zeros(len(self.C))
         deltaz = np.zeros(len(self.C))
-        zsum = 0
         for i in range(len(self.C)):
+            zsum = 0
             for j in range(len(self.G)):
                 zsum += self.S[i, j] * self.G[j] - self.C[i]
             z[i] = zsum
-            if z[i] <= 0:
-                deltaz[i] = 0
-            else:
-                deltaz[i] = self.alpha(self.t) * z[i]
+            if z[i] > 0: deltaz[i] = self.alpha(self.t) * z[i]
         return z, deltaz
 
     # Objektfunktion f1 nach S. 2203 (14)
@@ -138,10 +157,33 @@ class InitModel:
     # Spratte: Lösung für Berechnung der smoothMat (S.2202):
     # Unterstellt: Rechteckiges Bild mit Breite self.G_width, G[0] in linker
     # oberer Ecke, G[1] der zweite Pixel von links in der obersten Reihe
-    #def smoothMat(self):
-        #X = np.array(
-                #[ [ neighbours(i,j) for i in range(self.G.size) ]
-                    #for j in range(self.G.size) ] )
+    # (Reihenfolge eigentlich nicht wirklich wichtig, bloß G[0] in einer Ecke
+    # und self.G_width die Breite in die erste Richtung -- also
+    # G[self.G_width-1] der letzte Pixel in der ersten Reihe/Spalte). Die
+    # Funktion skaliert allerdings relativ schlecht (O(n) = n**2)
+    # Spratte: Für große Matrizen wird extrem viel Speicher benötigt. Auf meinem
+    # Laptop ist bei 200x200 Pixeln Bildgröße schluss. Eventuell müssen wir auf
+    # langsamere aber speicherfreundlichere Berechnungen umsteigen -- oder aber
+    # ich schreibe doch noch ein Modul in C.
+    # Die Speichermenge wechst in 4ter Potenz mit der Bildbreite, bzw.
+    # quadtratisch zur Pixelzahl, wobei ein 1x1 np.array 8 byte benötigt.
+    #def smoothMat2(size,width):
+        #X = np.zeros((size,size))
+        #for i in range(size):
+            #X[i][i] = 1 #x1
+            #for j in range(i+1,size):
+                #i_row = i // width # Python 3 Syntax für Integerdivision
+                #i_col = i %  width
+                #j_row = j // width # Python 3 Syntax für Integerdivision
+                #j_col = j %  width
+                #if   i_row == j_row          and abs(i_col - j_col) == 1 :
+                    #X[i,j] = -1/8 #x2
+                #elif abs(i_row - j_row) == 1 and i_col == j_col :
+                    #X[i,j] = -1/8 #x2
+                #elif abs(i_row - j_row) == 1 and abs(i_col - j_col) == 1 :
+                    #X[i,j] = -1/8 #x3
+                #X[j,i] = X[i,j]
+        #return X
     #def neighbours(self,i,j):
         #x1 = 1
         #if i == j: return x1
@@ -155,8 +197,51 @@ class InitModel:
         #if abs(i_row - j_row) == 1 and i_col == j_col          : return x2
         #if abs(i_row - j_row) == 1 and abs(i_col - j_col) == 1 : return x3
         #return 0
+    # Berechnet das Matrixprodukt X*G
+    def smoothie(self, G):
+        XG = self.smoothing_weights[0] * G.copy()
+        for i in range(XG.size):
+            E, V = get_neighbours(i) # returns 2 lists of indices
+            XG[i] +=   self.smoothing_weights[1] * np.sum(G[E]) \
+                     + self.smoothing_weigths[2] * np.sum(G[V])
+        return XG
+    def get_neighbours(self, i):
+        E = []
+        V = []
+        i_row = i // self.G_width
+        i_col = i - self.G_width * i_row # faster than modulo
+        if i_row != 0:
+            E.append(i - self.G_width)
+            if i_col != 0:
+                V.append(i - self.G_width - 1)
+            if i_col != self.G_width - 1:
+                V.append(i - self.G_width + 1)
+        if i_row != self.G_height - 1:
+            E.append(i + self.G_width)
+            if i_col != 0:
+                V.append(i + self.G_width - 1)
+            if i_col != self.G_width - 1:
+                V.append(i + self.G_width + 1)
+        if i_col != 0:
+            E.append(i - 1)
+        if i_col != self.G_width - 1:
+            E.append(i + 1)
+        return (E, V)
 
     # Smoothing Matrix (S. 2202)
+    # Spratte: Habe einen kurzen Test gemacht und es scheint, deine Funktion
+    # schmeißt Errors um sich. Ich habe getestet mit:
+    #IssueCount = 0
+    #for i in range(201):
+        #try:
+            #X = smoothMat(i**2)
+        #except:
+            #print("Issue with", i)
+            #IssueCount += 1
+    #print("Issues in ",IssueCount," iterations")
+    # und nur für 4 (also smoothMat(16)) funktioniert die Funktion. Das Argument
+    # war in diesem Fall len(self.G) (in meiner Testumgebung habe ich
+    # dementsprechend alle Aufrufe ersetzt).
     def smoothMat(self):
         X = np.zeros((len(self.G), len(self.G)))
         ind = np.array(range(0, len(self.G))).reshape(int(np.sqrt(len(self.G))), int(np.sqrt(len(self.G))))
@@ -261,8 +346,8 @@ class InitModel:
     #     f(u_j) = <  beta u_j + xi   if 0 <  beta u_j + xi < 1
     #               \ 1               if 1 <= beta u_j + xi
     #
-    # Das stimmt mit der Definition aus dem Paper überein, solange b und x in
-    # einem bestimmten Intervall liegen
+    # Das stimmt mit der Definition aus dem Paper überein, solange beta und xi
+    # in einem bestimmten Intervall liegen (genauer: solange beta = 1).
     # Sollte meine Vermutung richtig sein, ist folgende Implementierung ein
     # bisschen schneller:
     #def activation(self, u):

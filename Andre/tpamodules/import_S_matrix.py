87883307db41bab80e4d18f3084f5d15f4e37b84
Jonathan Spratte <jspratte@yahoo.de>
2018-11-08 11:59:26 +0100

    choosable subentry

--- Andre/tpamodules/import_S_matrix.py
+++ Andre/tpamodules/import_S_matrix.py
@@ -1,11 +1,11 @@
 from scipy.io import loadmat
 import numpy as np
 
-def importMatFile(pfad):
+def importMatFile(pfad,subentry=2):
     # f = h5py.File(pfad,'r') Für Matlab Versionen 7.3+ (ab Dezember 2015)
     S = loadmat(pfad)
     mat_keys = list(S)
     if S[mat_keys[-1]].shape == (1,1):
-        return S[mat_keys[-1]][0,0][0]
+        return S[mat_keys[-1]][0,0][subentry]
     else:
         return S[mat_keys[-1]]
